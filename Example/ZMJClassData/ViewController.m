83ed856f90d893948d6ed9932550a9cbaa5c2f39
zhengmingjun <keshiim@163.com>
2018-02-02 13:39:22 +0800

    add classdata stuff

--- Example/ZMJClassData/ViewController.m
+++ Example/ZMJClassData/ViewController.m
@@ -9,9 +9,26 @@
 #import "ViewController.h"
 #import <ZMJGanttChart/ZMJGanttChart.h>
 
+typedef NS_ENUM(NSInteger, ZMJSorting) {
+    ascending = 0,
+    descending
+};
 
-@interface ViewController ()
+static inline NSString * getSymbol(ZMJSorting sorting) {
+    switch (sorting) {
+        case ascending:
+            return @"\u{25B2}";
+            break;
+        case descending:
+            return @"\u{25BC}";
+            break;
+        default:
+            break;
+    }
+}
 
+@interface ViewController () <SpreadsheetViewDelegate, SpreadsheetViewDataSource>
+@property (nonatomic, strong) SpreadsheetView *spreadsheetView;
 @end
 
 @implementation ViewController
@@ -25,6 +42,29 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (SpreadsheetView *)spreadsheetView {
+    if (!_spreadsheetView) {
+        _spreadsheetView = ({
+            SpreadsheetView *ssv = [SpreadsheetView new];
+            ssv.dataSource = self;
+            ssv.delegate   = self;
+            ssv.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+            [self.view addSubview:ssv];
+            ssv;
+        });
+    }
+    return _spreadsheetView;
+}
+
+- (void)viewWillLayoutSubviews {
+    if (@available(iOS 11.0, *)) {
+        self.spreadsheetView.frame = self.view.safeAreaLayoutGuide.layoutFrame;
+    } else {
+        // Fallback on earlier versions
+        self.spreadsheetView.frame = self.view.bounds;
+    }
 }
 
 
